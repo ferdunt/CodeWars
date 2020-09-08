3ab1828fcaa212b39307efce9a37303292df6e3d
Fernando Guisbert Segales <guisbertfernando@gmail.com>
2020-09-07 20:46:03 -0400

    Word values loaded

---
+++ 7 kyu/Word values/README.md
@@ -0,0 +1,37 @@
+# WORD VALUES - 7kyu
+
+👨🏻‍💻 Solution using Javascript 👨🏻‍💻
+
+## Description
+
+You will be given a list of strings and your task will be to return the values of the strings multiplied by the position of that string in the list. For our purpose, position begins with 1.
+
+## Input
+
+```
+🥚 ["abc", "abc abc"] 🥚 `Array - Strings`
+```
+
+## Output
+
+```
+🐣 [6,24] 🐣 `Array - Integers`
+```
+
+## Explanation
+
+```
+ 1# ["abc", "abc abc"]
+ 2# [[1,2,3], [1,2,3,1,2,3]]
+ 3# [6, 12]
+ 4# [6*1, 12*2]
+ 5# [6, 24] 🎉
+```
+
+## Note
+
+⚠ Input will only contain lowercase characters and spaces. ⚠
+
+## Links
+
+🔗 [Square Every Digit](https://www.codewars.com/kata/598d91785d4ce3ec4f000018) 🔗
---
+++ 7 kyu/Word values/solution.js
@@ -0,0 +1,31 @@
+function wordValue(words) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let values = [];
+
+    // 💬 Loop the primary array 'words' 
+    for (let i = 0; i < words.length; i++) {
+        // 💬 Delete empty spaces and split into a new array
+        let secondaryArray = words[i].replace(' ', '').split('');
+        let sum = 0;
+
+        // 💬 Loop the secondary array to get its number values
+        secondaryArray.forEach((letter, index) => {
+            sum += alphabet.indexOf(secondaryArray[index]) + 1;
+        });
+
+        // 💬 Push the total 'sum' into the 'values' array
+        values.push(sum);
+    }
+
+    // 💬 Loop the final 'values' and multiply each one based on its index + 1
+    values.forEach((number, index) => {
+        values[index] = number * (index + 1);
+    });
+
+    return values; // 🐣 values => [6, 24] 
+
+}
+
+const result = wordValue(["abc", "abc abc"]);
+console.log(result); // 🐣 [6, 24] 
+
