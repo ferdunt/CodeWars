83b86858982d142cf3b77815b83bab113f4a2656
Fernando Guisbert Segales <guisbertfernando@gmail.com>
2020-09-16 09:56:26 -0400

    add solution

---
+++ 7 kyu/16+18=214/README.md
@@ -0,0 +1,31 @@
+# 16 + 18 = 214 - 7kyu
+
+The solution to this problem is written in JavaScript the detail about the problem is below and you can check the solution above.
+
+## 💬 Description 💬
+
+Make a function that will return an add but it does not like the principle of carrying over numbers and just writes down every number it calculates.
+
+## 🥚 Input 🥚
+
+```
+ (2, 11) (Integers)
+```
+
+## 🐣 Output 🐣
+
+```
+ 13 (Integer)
+```
+
+## 👓 Explanation 👓
+
+```
+ 1# 2 + 11
+ 2# 1 + 0 = 1, 1 + 2 = 3
+ 3# 13
+```
+
+## 🔗 Links 🔗
+
+[16+18=214](https://www.codewars.com/kata/5effa412233ac3002a9e471d)
---
+++ 7 kyu/16+18=214/solution.js
@@ -0,0 +1,37 @@
+function add(num1, num2) {
+    // 💬 Convert each integer into an array of integers
+    // 💬 map(Number) is equal to map(element => Number(element)) 😉
+    let array1 = num1.toString().split('').map(Number);
+    let array2 = num2.toString().split('').map(Number);
+
+    // 💬 Check if both arrays are the same size
+    while (array1.length !== array2.length) {
+        // 💬 Push one '0' at the beginning of the array to match both arrays
+        if (array1.length < array2.length) {
+            array1.unshift(0);
+        } else {
+            array2.unshift(0);
+        }
+    }
+
+    // 💬 'sum' will add the values as Integer
+    let sum = 0;
+
+    // 💬 'total' will concatenate the sum as String
+    let total = '';
+
+    // 💬 'size' is a general size of both arrays
+    const size = array1.length;
+
+    // 💬 Add the numbers based in its position and concatenate
+    for (let i = 0; i < size; i++) {
+        sum = array1[i] + array2[i];
+        total += sum.toString();
+    }
+
+    // 💬 Convert the 'string' into a 'Number' and return
+    return Number(total);
+}
+
+const result = add(2, 11) // 🥚
+console.log(result); // 🐣 13
